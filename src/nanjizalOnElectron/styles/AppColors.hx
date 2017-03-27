9eca0d42423041c9984b41639a73afeffb36f3b5
nanjizal <nitsuj@nanjizal.net>
2017-03-27 23:54:21 +0100

    Create AppColors.hx

---
+++ src/nanjizalOnElectron/styles/AppColors.hx
@@ -0,0 +1,10 @@
+package nanjizalOnElectron.styles;
+
+class AppColors {
+    public static inline var background: String = '#2B2E3B';// Electron dark blue grey background
+    public static inline var bodygrond: String = '#2F3242';// Electron dark blue grey body ( lighter )
+    public static inline var highlight: String = '#7281BF'; // Electron highlight 
+    public static inline var highlightBright: String = '#9CEAFA';// Electron highlight bright
+    public static inline var highlightDim: String = '#50727E'; // Electron highlight dim
+    
+}
